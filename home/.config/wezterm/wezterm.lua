3d95defa82efda30d6391897613e4227644fc25e
Matt McMahand <matt@invalid8.com>
2022-10-18 13:02:08 -0400

    toying around with wezterm

--- home/.config/wezterm/wezterm.lua
+++ home/.config/wezterm/wezterm.lua
@@ -1,12 +1,21 @@
 local wezterm = require 'wezterm'
+
 local act = wezterm.action
+local mux = wezterm.mux
+
+wezterm.on('gui-startup', function(cmd)
+  local tab, pane, window = mux.spawn_window(cmd or {})
+  window:gui_window():set_position(140, 300)
+end)
 
 return {
   color_scheme = "Solarized Dark Higher Contrast",
-  font_size = 16
+  font_size = 14,
+
   keys = {
     { key = 'k', mods = 'CMD', action = act.ClearScrollback 'ScrollbackAndViewport' }
-  }
-  -- debug_key_events = true,
-}
+  },
 
+  --initial_cols = 200,
+  --initial_rows = 65,
+}
