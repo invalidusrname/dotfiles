115f6d6fb64e2a77c3052f483be8393895764c12
Matt McMahand <matt@invalid8.com>
2023-11-25 14:26:07 -0500

    load homebrew and install some minimal packages

--- scripts/mac_setup.sh
+++ scripts/mac_setup.sh
@@ -1,6 +1,7 @@
 if ! command -v brew &> /dev/null; then
   echo "installing brew..."
  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+ eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
 #curl -L https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh > ~/.bash_git
@@ -8,6 +9,6 @@ fi
 
 if ! command -v asdf &> /dev/null
 then
-  echo "installing asdf..."
-  brew install asdf
+  echo "installing minimal brews..."
+  brew install iterm2 1password asdf
 fi
